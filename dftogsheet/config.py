2fbf38c7da4aeb5847d9d23f7b711e60536b7e4f
William Dry <wpbdry@gmail.com>
2019-06-08 16:11:02 +0200

    add support for custom config

--- dftogsheet/config.py
+++ dftogsheet/config.py
@@ -1,9 +1,43 @@
+import os.path
+import json
+
+'''
+Default configurations:
+'''
 # If modifying these scopes, delete the file token.pickle.
 scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
 # Type of input
+# Documentation https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
 value_input_option = 'RAW'
 
-# Paths to files
+# Paths to credentials files
 token = '.dftogsheet/credentials/token.pickle'
 credentials = '.dftogsheet/credentials/credentials.json'
+
+# Path to config file
+with open('dftogsheet/config-location.json') as config_location:  
+    config = json.load(config_location)['path']
+
+'''
+Set project specific variables if they exist
+'''
+if os.path.exists(config):
+    with open(config) as config:
+        project_config = json.load(config)
+        try:
+            scopes = project_config['scopes']
+        except:
+            pass
+        try:
+            value_input_option = project_config['valueInputOption']
+        except:
+            pass
+        try:
+            token = project_config['token']
+        except:
+            pass
+        try:
+            credentials = project_config['credentials']
+        except:
+            pass
